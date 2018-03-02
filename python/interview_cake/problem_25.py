35725c3201bb3b13a281b220205235ec5933089c
ebehlmann <ebehlmann@gmail.com>
2018-03-01 22:00:55 -0600

    fixing weird character issue

--- python/interview_cake/problem_25.py
+++ python/interview_cake/problem_25.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 """
-You have a linked list ↴ and want to find the kkth to last node.
+You have a linked list and want to find the kkth to last node.
 Write a function kth_to_last_node() that takes an integer kk and the head_node of a singly-linked list, and returns the kkth to last node in the list.
 """
 
