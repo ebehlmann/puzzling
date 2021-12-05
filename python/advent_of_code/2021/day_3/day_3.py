979c741cc9ebaefa90319145a08f2dc1b49a290a
ebehlmann <ebehlmann@gmail.com>
2021-12-05 12:51:05 -0600

    finish day 3

--- python/advent_of_code/2021/day_3/day_3.py
+++ python/advent_of_code/2021/day_3/day_3.py
@@ -11,16 +11,23 @@ def process_input(input_file):
 	return binaries
 
 
+def find_most_common_for_bit(binaries, index):
+	bits = []
+	for binary in binaries:
+		bits.append(binary[index])
+	occurence_count = Counter(bits)
+	# in case of a tie return 1
+	if occurence_count.most_common(2)[0][1] == occurence_count.most_common(2)[1][1]:
+		return 1
+	return occurence_count.most_common(1)[0][0]
+
+
 def get_gamma_string(binaries):
 	gamma = ''
 	i = 0
 	while i < len(binaries[0]):
-		bits = []
-		for binary in binaries:
-			bits.append(binary[i])
+		gamma += find_most_common_for_bit(binaries, i)
 		i += 1
-		occurence_count = Counter(bits)
-		gamma += occurence_count.most_common(1)[0][0]
 	return gamma
 
 
@@ -31,12 +38,32 @@ def get_epsilon_string(gamma):
 	return epsilon
 
 
-def calculate_power_consumption(gamma, epsilon):
-	return int(gamma, 2) * int(epsilon, 2)
+def multiply_binaries(one, two):
+	return int(one, 2) * int(two, 2)
+
+
+def get_rating(binaries, type):
+	i = 0
+	while i < len(binaries[0]):
+		value = str(find_most_common_for_bit(binaries, i))
+		if type == 'co2':
+			value = str(abs(int(value) - 1))
+		binaries = list(filter(lambda x: x[i] == value, binaries))
+		if len(binaries) == 1:
+			break
+		i += 1
+	return binaries[0]
 
 
 binaries = process_input('input.csv')
+
 gamma = get_gamma_string(binaries)
 epsilon = get_epsilon_string(gamma)
-power_consumption = calculate_power_consumption(gamma, epsilon)
-print(power_consumption)
+power_consumption = multiply_binaries(gamma, epsilon)
+print('Power consumption: {}'.format(power_consumption))
+
+oxygen_generator_rating = get_rating(binaries, 'oxygen')
+co2_scrubber_rating = get_rating(binaries, 'co2')
+life_support_rating = multiply_binaries(oxygen_generator_rating, co2_scrubber_rating)
+print('Life support rating: {}'.format(life_support_rating))
+
